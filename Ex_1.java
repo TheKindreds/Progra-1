6511082709f81af42248df42242e08f8fb4008a8
Ismael Zelaya <167560566+TheKindreds@users.noreply.github.com>
2024-08-04 22:22:08 -0600

    Add files via upload

---
+++ Ex_1.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zelay
+ */
+public class Ex_1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        String palabra;
+        char caracter;
+        System.out.println("Ingrese una palabra");
+        palabra = leer.next();
+        for (int i = 0; i < palabra.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            caracter = palabra.charAt(i);
+            System.out.println(caracter);
+        }
+
+        for (int k = palabra.length() - 1; k >= 0; k--) {
+            for (int j = 0; j < k; j++) {
+                System.out.print(" ");
+            }
+            caracter = palabra.charAt(k);
+            System.out.println(caracter);
+        }
+    }
+}
---
+++ Ex_2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+
+/**
+ *
+ * @author zelay
+ */
+public class Ex_2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Que numero de filas desea en la piramide");
+        int num = sc.nextInt();
+        for(int i=1 ; i<=num ;i++){
+                for(int o=1;o<i;o++){
+                    System.out.print("  ");
+                }
+                  for(int n=i;n<=num;n++){
+                      System.out.print(n+" ");
+                  }
+                   for(int nu=num-1;nu>=i;nu--){
+                       System.out.print(nu+" ");
+                   }
+                 System.out.println();
+            }
+        }
+    }
